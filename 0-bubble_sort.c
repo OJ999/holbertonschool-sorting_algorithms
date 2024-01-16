9e0786fe4c7751db91f9c71e00911a91ecea5970
Omar J <omarj819@gmail.com>
2024-01-16 16:59:29 +0200

    A

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,28 +9,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int flag, aux = 0; /* aux to make the swap */
-	size_t i, stop = (size - 1); /* stop: don't be out of the array */
+int flag, aux = 0; /* aux to make the swap */
+size_t i, stop = (size - 1); /* stop: don't be out of the array */
 
-	if (array == NULL || size < 2)
-		return;
-	for (i = 0; i < stop; i++)
-	{
-		flag = 0;
-		if (array[i] > array[i + 1])
-		{
-			aux = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = aux;
-			print_array(array, size);
-			flag = 1; /* a swap was made */
-		}
-		if (flag == 0 && i == stop - 1)  /* everything is in order */
-			return;
-		if (i == (stop - 1) && flag == 1)   /* last position */
-		{
-			i = -1;    /* start from the beginning */
-			stop--;   /* the last elements are in order */
-		}
-	}
+if (array == NULL || size < 2)
+return(0);
+for (i = 0; i < stop; i++)
+{
+flag = 0;
+if (array[i] > array[i + 1])
+{
+aux = array[i];
+array[i] = array[i + 1];
+array[i + 1] = aux;
+print_array(array, size);
+flag = 1; /* a swap was made */
+}
+if (flag == 0 && i == stop - 1)  /* everything is in order */
+return(0);
+if (i == (stop - 1) && flag == 1)   /* last position */
+{
+i = -1;    /* start from the beginning */
+stop--;   /* the last elements are in order */
+}
+}
 }
